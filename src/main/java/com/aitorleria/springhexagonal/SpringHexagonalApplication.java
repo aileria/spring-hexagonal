c65013e03ed63c92a2ed94356631058b2d9bc72e
Aitor Lería Fortea <aitorleria@gmail.com>
2022-05-16 10:12:58 +0200

    Initial commit

---
+++ src/main/java/com/aitorleria/springhexagonal/SpringHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.aitorleria.springhexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHexagonalApplication.class, args);
+	}
+
+}
